1484ac5037edb11f2c9e4491fe135c7ee678d315
cafemoloko <karolina.pawlaczyk@gmail.com>
2023-08-25 18:16:19 +0200

    MA, Volatility and Correlation

--- stock_market_performance.py
+++ stock_market_performance.py
@@ -30,3 +30,42 @@ fig_line.show() # Stock rerformance for the last 3 months, line chart
 fig_area.show() # Stock performance for the last 3 months, faceted area chart
 
 
+# Moving averages (MA)
+
+# When the MA10 crosses above the MA20, it indicates that the stock price will rise. Contrarily, if the MA10 crosses below 
+# the MA20 - the stock price will fall. 
+
+
+df['MA10'] = df.groupby('Ticker')['Close'].rolling(window=10).mean().reset_index(0, drop=True)
+df['MA20'] = df.groupby('Ticker')['Close'].rolling(window=20).mean().reset_index(0, drop=True)
+
+for ticker, group in df.groupby('Ticker'):
+    print(f'Moving Averages for {ticker}')
+    print(group[['MA10', 'MA20']])
+
+for ticker, group in df.groupby('Ticker'):
+    fig_ma = px.line(group, x='Date', y=['Close', 'MA10', 'MA20'],
+                     title=f"{ticker} Moving Averages")
+    fig_ma.show()
+
+
+# Volatility (a measure of how much and how often the stock price fluctuates over a given period of time)
+
+df['Volatility'] = df.groupby('Ticker')['Close'].pct_change().rolling(window=10).std().reset_index(0, drop=True)
+fig_vol = px.line(df, x='Date', y='Volatility', 
+              color='Ticker', 
+              title='Volatility of All Companies')
+fig_vol.show()
+
+
+# Correlation between NVIDIA and AMD stock prices
+# correlation (line): positive /, negative \, no correlation -
+
+amd = df.loc[df['Ticker'] == 'AMD', ['Date', 'Close']].rename(columns={'Close': 'AMD'})
+nvidia = df.loc[df['Ticker'] == 'NVDA', ['Date', 'Close']].rename(columns={'Close': 'NVDA'})
+
+df_corr = pd.merge(amd, nvidia, on='Date')
+
+fig_corr = px.scatter(df_corr, x='AMD', y='NVDA', trendline='ols',
+                      title='Correlation between AMD and NVIDIA')
+fig_corr.show()
